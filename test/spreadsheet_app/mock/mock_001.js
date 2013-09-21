ce0eb107d45e8e4d4443196663119fcebd091477
Hiroyuki Sano <sh19910711@gmail.com>
2013-09-21 09:31:05 +0900

    Add test T001_002

--- test/spreadsheet_app/mock/mock_001.js
+++ test/spreadsheet_app/mock/mock_001.js
@@ -1,15 +1,20 @@
-// 新しくスプレッドシートを作成して、データを設定し
-// 作成したシートを開いてデータを取得する
-function test_001() {
-  var spreadsheet = SpreadsheetApp.create("test");
-  var sheet_name = "test-sheet-01";
-  var new_sheet = spreadsheet.insertSheet();
-  new_sheet.setName(sheet_name);
-  new_sheet.getRange(1, 1).setValue(999);
-  var sheet = spreadsheet.getSheetByName(sheet_name);
-  return sheet.getRange(1, 1).getValue();
-}
-
 module.exports = {
-  test_001: test_001
+  // 新しくスプレッドシートを作成して、データを設定し
+  // 作成したシートを開いてデータを取得する
+  test_001: function() {
+    var spreadsheet = SpreadsheetApp.create("test");
+    var sheet_name = "test-sheet-01";
+    var new_sheet = spreadsheet.insertSheet();
+    new_sheet.setName(sheet_name);
+    new_sheet.getRange(1, 1).setValue(999);
+    var sheet = spreadsheet.getSheetByName(sheet_name);
+    return sheet.getRange(1, 1).getValue();
+  },
+  // スプレッドシートを作成してID指定で開き直す
+  test_002: function() {
+    var spreadsheet = SpreadsheetApp.create("test");
+    var id = spreadsheet.getId();
+    var sp = SpreadsheetApp.openById(id);
+    return sp.getName();
+  }
 };
--- test/spreadsheet_app/test_001.js
+++ test/spreadsheet_app/test_001.js
@@ -6,4 +6,7 @@ describe('T001: SpreadsheetApp', function() {
   it('T001_001: #create', function t_001_001() {
     mock.test_001().should.be.equal(999);
   });
+  it('T001_002: #create', function t_001_002() {
+    mock.test_002().should.be.equal("test");
+  });
 });
